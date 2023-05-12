449de6b2a97ebc597d75d19b352626a00b877788
TiverNine3301 <vlad.mikhalev2001@bk.ru>
2023-05-13 03:31:04 +0700

    проверка файла

---
+++ index.py
@@ -0,0 +1,20 @@
+# for i in range(0, 31, 3):
+#     print(i)
+
+a = int(input("Введите число:"))
+if a % 2 == 0:
+    print("xuy")
+else:
+    print("pizda")
+
+name = "Aleksey"
+for i in range(5):
+    print(name)
+
+# while
+#
+#     break
+#
+# if
+#     elif
+# else
